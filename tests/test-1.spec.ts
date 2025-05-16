b129a49107578a7ef362a5abdc3d7fda85f16002
Daniel J <daniel.jonsson@consid.se>
2025-05-16 15:33:38 +0200

    baseURL

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Navigation', () => {
   test('test', async ({ page }) => {
-    await page.goto('/');
+    const baseURL = process.env.BASE_URL;
+    await page.goto(baseURL);
     await page.getByRole('link', { name: 'edit' }).first().click();
     await expect(
       page.getByRole('button', { name: 'Update Post' })
