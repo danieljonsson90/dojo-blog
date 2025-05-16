ef85dadffa617d7ff5498354620831541183e4b1
Daniel J <daniel.jonsson@consid.se>
2025-05-16 11:28:56 +0200

    test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,15 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://brave-hill-0cffb3403.6.azurestaticapps.net/');
-  await page.getByRole('link', { name: 'edit' }).first().click();
-  await expect(page.getByRole('button', { name: 'Update Post' })).toBeVisible();
-  await page.getByRole('button', { name: 'Update Post' }).click();
-  await expect(page.getByText('"Perfect is the enemy of good"')).toBeVisible();
+test.describe('Navigation', () => {
+  test('test', async ({ page }) => {
+    await page.goto('https://brave-hill-0cffb3403.6.azurestaticapps.net/');
+    await page.getByRole('link', { name: 'edit' }).first().click();
+    await expect(
+      page.getByRole('button', { name: 'Update Post' })
+    ).toBeVisible();
+    await page.getByRole('button', { name: 'Update Post' }).click();
+    await expect(
+      page.getByText('"Perfect is the enemy of good"')
+    ).toBeVisible();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Create' }).click();
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Create' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('button', { name: 'Add Post' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('button', { name: 'Add Post' })).toBeVisible();
+  });
 });
